a45055df64226da7bdd3ebf5c44968b37e102493
Dimitri Kurashvili <dimakura@gmail.com>
2014-09-25 18:21:53 +0400

    create admin application on startup

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,10 @@
 # -*- encoding : utf-8 -*-
 class AdminController < ApplicationController
-  before_filter :admin_area
+  before_filter :adminapp_startup
 
   protected
 
-  def admin_area
-    @header_addition = 'Admin'
-    # TODO: check admin privileges
+  def adminapp_startup
+    @application = 'admin'
   end
 end
