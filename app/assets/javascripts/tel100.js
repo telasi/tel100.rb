c8868ad60aade5016276d8fc4a565835bfdcb66e
Dimitri Kurashvili <dimakura@gmail.com>
2014-09-29 21:22:13 +0400

    using Ctl+arrows for moving around pagination

--- app/assets/javascripts/tel100.js
+++ app/assets/javascripts/tel100.js
@@ -1,15 +1,43 @@
-var Tel100 = {
+var Tel100 = (function() {
 
-  historyBack: function(evt) {
-    evt.preventDefault();
-    try {
-      window.history.back();
-    } catch(e) {
-      console.log(e);
-      var $this = $(this);
-      var href = $this && typeof $this.attr === 'function' && $this.attr('href')
-      if (href) { window.location = href; }
+  var pageIsLoading;
+
+  var openUrl = function(url) {
+    if ( !pageIsLoading ) {
+      window.location = url;
+      pageIsLoading = true;
     }
-  }
+  };
 
-};
+  var goBack = function() {
+    if ( !pageIsLoading ) {
+      window.history.back();
+      pageIsLoading = true;
+    }
+  };
+
+  return {
+    historyBack: function(evt) {
+      evt.preventDefault();
+      try {
+        goBack();
+      } catch(e) {
+        var $this = $(this);
+        var url = $this && typeof $this.attr === 'function' && $this.attr('href')
+        if (url) { openUrl(url); }
+      }
+    },
+
+    paginateNavigation: function(evt) {
+      evt.preventDefault();
+      if (evt.keyCode === 37 && evt.ctrlKey) {
+        var url = $('.pagination .prev a').attr('href');
+        if (url) { openUrl(url); }
+      } else if (evt.keyCode === 39 && evt.ctrlKey) {
+        var url = $('.pagination .next a').attr('href');
+        if (url) { openUrl(url); }
+      }
+    },
+  };
+
+})();
