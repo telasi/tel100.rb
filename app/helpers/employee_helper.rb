4241f8e69fb8cad49caecc6fb2030e4c73d09709
Dimitri Kurashvili <dimakura@gmail.com>
2014-09-29 20:32:04 +0400

    show employee popover

---
+++ app/helpers/employee_helper.rb
@@ -0,0 +1,7 @@
+module EmployeeHelper
+
+  def employee_org_chain(empl)
+    empl.organization.chain.map{|x| "<span><code>#{x.saporg_id}</code> #{x.name}</span>"}.join('<hr style="margin: 5px 0; width: 600px;">')
+  end
+
+end
