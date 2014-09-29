d2981cb53faacda6c29f8ac97dda0bf7f08d0db9
Dimitri Kurashvili <dimakura@gmail.com>
2014-09-29 19:40:35 +0400

    link employee with organization

--- app/models/hr/employee.rb
+++ app/models/hr/employee.rb
@@ -3,6 +3,7 @@ class HR::Employee < ActiveRecord::Base
   self.table_name  = 'hr_employees'
   self.sequence_name = 'hr_employees_seq'
   self.localized_fields('first_name', 'last_name')
+  belongs_to :organization, class_name: 'HR::Organization'
 
   def person_number; self.person_id.to_s.rjust(5, '0') end
   def full_name; "#{first_name} #{last_name}" end
--- app/models/hr/organization.rb
+++ app/models/hr/organization.rb
@@ -2,4 +2,5 @@
 class HR::Organization < ActiveRecord::Base
   self.table_name  = 'hr_organizations'
   self.sequence_name = 'hr_orgs_seq'
+  self.localized_field('name')
 end
