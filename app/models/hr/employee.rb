824bae96a0072267acd25ad9553a22655ed8189a
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-02 17:58:07 +0400

    first version of employee gem using forma

--- app/models/hr/employee.rb
+++ app/models/hr/employee.rb
@@ -9,7 +9,7 @@ class HR::Employee < ActiveRecord::Base
 
   def person_number; self.person_id.to_s.rjust(5, '0') end
   def full_name; "#{first_name} #{last_name}" end
-  def active?; self.is_active end
+  def active?; self.is_active == 1 end
   def employee_status; I18n.t("models.hr_employee.employee_status_id.val#{self.employee_status_id}") end
 
   def self.active; HR::Employee.where(is_active: 1) end
