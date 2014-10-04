f788214707700991ff11d0d043355980b40d0fb6
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-04 17:22:54 +0400

    default empty text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # -*- encoding : utf-8 -*-
 module ApplicationHelper
+
+  def empty_text(models = nil, url = nil)
+    if url and models.respond_to?(:total_entries) and models.total_entries > 0
+      t('forma.no_data_p1', url: url)
+    else
+      t('forma.no_data')
+    end
+  end
+
 end
