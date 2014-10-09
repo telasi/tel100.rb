83186790166629128da11850721a3b8ee52d80ae
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-09 14:11:08 +0400

    removing old application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
-  include Authentification
-  include UrlManager
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,6 @@
 # -*- encoding : utf-8 -*-
 Rails.application.routes.draw do
+  # TODO: api
 
-  namespace :admin do
-    get '/', controller: 'home', action: 'index', as: 'home'
-    scope '/users', controller: 'users' do
-      get '/', action: 'index', as: 'users'
-      get '/show/:id', action: 'show', as: 'user'
-      match '/new', action: 'new', as: 'new_user', via: ['get', 'post']
-      match '/edit/:id', action: 'edit', as: 'edit_user', via: ['get', 'post']
-    end
-    scope '/employees', controller: 'employees' do
-      get '/', action: 'index', as: 'employees'
-      get '/show/:id', action: 'show', as: 'employee'
-      post '/sync', action: 'sync', as: 'sync_employees'
-    end
-  end
-
-  namespace :tel100 do
-    scope '/', controller: 'home' do
-      get '/', action: 'index', as: 'documents'
-      match '/new', action: 'new', as: 'new_document', via: ['get', 'post']
-    end
-  end
-
-  scope controller: 'site' do
-    match '/login', action: 'login', as: 'login', via: ['get','post']
-    match '/register', action: 'register', as: 'register', via: ['get', 'post']
-    root action: 'index', as: 'home'
-  end
-
-  get '/tel100', to: 'site#index'
-
+  root controller: 'site', action: 'index'
 end
