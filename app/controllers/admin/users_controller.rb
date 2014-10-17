0e2b170af1d866649094a233481273da110c145b
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-17 15:27:04 +0400

    edit user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,6 +10,16 @@ class Admin::UsersController < AdminController
     @title = @user.full_name
   end
 
+  def edit
+    @title = 'მომხმარებლის შეცვლა'
+    @user = Sys::User.find(params[:id])
+    if request.post?
+      if @user.update_attributes(params.require(:sys_user).permit(:username, :virtual_password, :email, :mobile, :phone))
+        redirect_to admin_user_url(id: @user.id)
+      end
+    end
+  end
+
   def destroy
     user = Sys::User.find(params[:id])
     user.destroy
