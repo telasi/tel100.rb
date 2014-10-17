c6ced289368519d26de385649c4aad396c85b43d
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-17 10:28:33 +0400

    don't paginate employees

--- app/controllers/admin/hr_controller.rb
+++ app/controllers/admin/hr_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HrController < AdminController
   def index
     @title = 'თანამშრომლები'
-    @employees = HR::Employee.active.order('person_id').paginate(page: params[:page], per_page: 10)
+    @employees = HR::Employee.active.order('person_id')
   end
 
   def employee
