6295b93816ee761888e50f01c59e62a6a9af48e2
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-20 13:09:21 +0400

    simplify center panel

--- public/js/app/view/common/center/CenterPanel.js
+++ public/js/app/view/common/center/CenterPanel.js
@@ -1,29 +1,18 @@
 Ext.define('Telasi.view.common.center.CenterPanel', {
-    extend: 'Ext.panel.Panel',
-    xtype: 'centerpanel',
-    layout: 'border',
+  extend: 'Ext.panel.Panel',
+  xtype: 'centerpanel',
+  layout: 'border',
 
-    requires: [
-        'Telasi.view.common.center.SearchPanel',
-        'Telasi.view.common.document.Grid'
-    ],
+  requires: [
+    'Telasi.view.common.center.SearchPanel',
+    'Telasi.view.common.document.Grid'
+  ],
 
-    initComponent: function(){
-        Ext.apply(this, {
-            items: [
-                {
-                    xtype: 'searchpanel',
-                    region: 'north',
-                },
-                {
-                    xtype: 'docgrid',
-                    region: 'center'
-                }
-            ]
-        });
-        
-        this.callParent();
-    }
-
-    
+  items: [{
+    xtype: 'searchpanel',
+    region: 'north',
+  }, {
+    xtype: 'docgrid',
+    region: 'center'
+  }],
 });
