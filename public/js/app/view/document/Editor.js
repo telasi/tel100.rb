98147f9a935e3e9dd26f074bb054b38414407c6a
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-20 18:02:19 +0400

    document editor changes

--- public/js/app/view/document/Editor.js
+++ public/js/app/view/document/Editor.js
@@ -1,9 +1,40 @@
 Ext.define('Telasi.view.document.Editor', {
   extend: 'Ext.form.Panel',
   alias: 'widget.documentEditor',
+  requires: [
+    'Telasi.model.document.Type',
+    'Telasi.store.document.Type',
+  ],
   // controller: 'documentEditor',
 
   title: 'ახალი დოკუმენტი',
-  html: 'TODO: document editor...',
   closable: true,
+
+  items: [{
+    xtype: 'panel',
+    layout: {
+      type: 'hbox',
+      align: 'stretch',
+      padding: 5
+    },
+    items: [{
+      xtype: 'combo',
+      store: 'documentTypes',
+      displayField: 'name',
+      valueField: 'id',
+      editable: false,
+      autoSelect: true,
+      emptyText: 'სახეობა',
+      padding: '0 5px 0 0'
+    }, {
+      xtype: 'datefield',
+      emptyText: 'თარიღი',
+      format: 'd/m/Y',
+      padding: '0 5px 0 0'
+    }, {
+      xtype: 'textfield',
+      emptyText: 'ნომერი',
+      flex: 1
+    }, ],
+  }],
 });
