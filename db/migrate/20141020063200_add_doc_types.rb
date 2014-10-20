327bdd2d8ee7714b51e386dd0b54b0c75e2465b8
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-20 10:42:40 +0400

    adding document types table

---
+++ db/migrate/20141020063200_add_doc_types.rb
@@ -0,0 +1,36 @@
+class AddDocTypes < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      create table DOC_TYPES (
+        ID number(5, 0) not null,
+        NAME_KA VARCHAR2(50) not null,
+        NAME_RU VARCHAR2(50),
+        NAME_END VARCHAR2(50),
+        ORDER_BY NUMBER(5, 0) default 0 not null,
+        constraint DOCTYPES_PRIMARYKEY primary key ( ID ) enable
+      )
+    SQL
+
+    execute <<-SQL
+      create sequence DOCTYPES_SEQ increment by 1 start with 1 nocache
+    SQL
+
+    execute <<-SQL
+      create trigger DOCTYPES_BEFORE_INSERT
+      before insert on DOC_TYPES
+      for each row
+      BEGIN
+        IF :new.ID is null
+        THEN
+          :new.ID := DOCTYPES_SEQ.nextval;
+        END IF;
+      END;
+    SQL
+  end
+
+  def down
+    execute "drop trigger DOCTYPES_BEFORE_INSERT"
+    execute "drop sequence DOCTYPES_SEQ"
+    execute "drop table DOC_TYPES"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009061501) do
+ActiveRecord::Schema.define(version: 20141020063200) do
+
+  create_table "doc_types", force: true do |t|
+    t.string  "name_ka",  limit: 50,                                     null: false
+    t.string  "name_ru",  limit: 50
+    t.string  "name_end", limit: 50
+    t.integer "order_by", limit: 5,  precision: 5, scale: 0, default: 0, null: false
+  end
 
   create_table "document_texts", primary_key: "document_id", force: true do |t|
     t.text "body"
@@ -106,8 +113,8 @@ ActiveRecord::Schema.define(version: 20141009061501) do
     t.boolean   "mobile_confirmed",             precision: 1,  scale: 0, default: false, null: false
     t.boolean   "is_active",                    precision: 1,  scale: 0, default: true,  null: false
     t.boolean   "is_admin",                     precision: 1,  scale: 0, default: false, null: false
-    t.integer   "employee_id",      limit: 10,  precision: 10, scale: 0
-    t.integer   "person_id",        limit: 8,   precision: 8,  scale: 0
+    t.integer   "employee_id",      limit: 10,  precision: 10, scale: 0,                 null: false
+    t.string    "person_id",        limit: 8,                                            null: false
     t.string    "first_name_ka",    limit: 50,                                           null: false
     t.string    "first_name_ru",    limit: 50
     t.string    "first_name_en",    limit: 50
