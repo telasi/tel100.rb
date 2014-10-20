6dfa402e5c607ca26a62ab7d7d8f516d577b75f1
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-20 13:09:43 +0400

    adding folders controller

--- public/js/app/view/document/Folders.js
+++ public/js/app/view/document/Folders.js
@@ -1,17 +1,24 @@
 Ext.define('Telasi.view.document.Folders', {
   extend: 'Ext.form.Panel',
   alias: 'widget.documentFolders',
+  requires: [
+    'Telasi.view.document.FoldersController'
+  ],
+  controller: 'documentfolders',
+
   html: 'this is west',
   width: 300,
   split: true,
   collapsible: true,
   title: '<i class="fa fa-bars"></i> სტატუსები',
+
   dockedItems: [{
     dock: 'top',
     xtype: 'toolbar',
     items: [{
       xtype: 'button',
-      html: '<i class="fa fa-plus"></i> ახალი დოკუმენტი'
+      html: '<i class="fa fa-plus"></i> ახალი დოკუმენტი',
+      handler: 'onNewDocument'
     }]
   }]
 });
---
+++ public/js/app/view/document/FoldersController.js
@@ -0,0 +1,8 @@
+Ext.define('Telasi.view.document.FoldersController', {
+  extend : 'Ext.app.ViewController',
+  alias: 'controller.documentfolders',
+
+  onNewDocument: function(button) {
+    console.log('onNewDocument');
+  },
+});
