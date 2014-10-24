fec3d6e80c868ba1a885e47e27259a9b55b90482
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-24 09:53:52 +0400

    changes in document.Base class

--- public/js/app/model/document/Base.js
+++ public/js/app/model/document/Base.js
@@ -1,62 +1,25 @@
 Ext.define('Telasi.model.document.Base', {
-    extend: 'Ext.data.Model',
-    idProperty: 'id',
-    fields: [
-        'id',
-        'language',
-        { name: 'typeId', type: 'int' },
-        { name: 'pageCount', type: 'int' },
-        { name: 'additionsCount', type: 'int' },
-        'direction',
-        'subject',
-        { name: 'body', mapping: 'text.body' },
-        'docnumber',
-        'docdate',
-        'dueDate',
-        'alertDate',
-        'status',
-        'author_id',
-        'author_user',
-        { name: 'author_first_name_ka', mapping: 'author_user.first_name_ka'},
-        { name: 'author_last_name_ka', mapping: 'author_user.last_name_ka'},
-        { name: 'author_first_name_ru', mapping: 'author_user.first_name_ru'},
-        { name: 'author_last_name_ru', mapping: 'author_user.last_name_ru'},
-        { name: 'author_first_name_en', mapping: 'author_user.first_name_en'},
-        { name: 'author_last_name_en', mapping: 'author_user.last_name_en'},
-        {
-            name: 'author_fullname',
-            convert: function(val, record){
-               // return record.get('first_name_'+Telasi.app.globals.locale) + ' ' + record.get('last_name_'+Telasi.app.globals.locale);
-               return record.get('author_first_name_ka') + ' ' + record.get('author_last_name_ka');
-            }
-        },
-        'sender_user',
-        { name: 'sender_first_name_ka', mapping: 'sender_user.first_name_ka'},
-        { name: 'sender_last_name_ka', mapping: 'sender_user.last_name_ka'},
-        { name: 'sender_first_name_ru', mapping: 'sender_user.first_name_ru'},
-        { name: 'sender_last_name_ru', mapping: 'sender_user.last_name_ru'},
-        { name: 'sender_first_name_en', mapping: 'sender_user.first_name_en'},
-        { name: 'sender_last_name_en', mapping: 'sender_user.last_name_en'},
-        {
-            name: 'sender_fullname',
-            convert: function(val, record){
-               // return record.get('first_name_'+Telasi.app.globals.locale) + ' ' + record.get('last_name_'+Telasi.app.globals.locale);
-               return record.get('sender_first_name_ka') + ' ' + record.get('sender_last_name_ka');
-            }
-        },
-        'owner_user',
-        { name: 'owner_first_name_ka', mapping: 'owner_user.first_name_ka'},
-        { name: 'owner_last_name_ka', mapping: 'owner_user.last_name_ka'},
-        { name: 'owner_first_name_ru', mapping: 'owner_user.first_name_ru'},
-        { name: 'owner_last_name_ru', mapping: 'owner_user.last_name_ru'},
-        { name: 'owner_first_name_en', mapping: 'owner_user.first_name_en'},
-        { name: 'owner_last_name_en', mapping: 'owner_user.last_name_en'},
-        {
-            name: 'owner_fullname',
-            convert: function(val, record){
-               // return record.get('first_name_'+Telasi.app.globals.locale) + ' ' + record.get('last_name_'+Telasi.app.globals.locale);
-               return record.get('owner_first_name_ka') + ' ' + record.get('owner_last_name_ka');
-            }
-        }
-    ]
+  extend: 'Ext.data.Model',
+  idProperty: 'id',
+  fields: [
+    { name: 'id', type: 'int' },
+    { name: 'language' },
+    { name: 'typeId', type: 'int' },
+    { name: 'pageCount', type: 'int' },
+    { name: 'additionsCount', type: 'int' },
+    { name: 'direction' },
+    { name: 'subject' },
+    { name: 'body', mapping: 'text.body' },
+    { name: 'docnumber' },
+    { name: 'docdate' },
+    { name: 'dueDate' },
+    { name: 'alertDate' },
+    { name: 'status' },
+    { name: 'senderUserId', type: 'int' },
+    { name: 'senderId', type: 'int' },
+    { name: 'senderType' },
+    { name: 'ownerUserId', type: 'int' },
+    { name: 'ownerId', type: 'int' },
+    { name: 'ownerType' },
+  ]
 });
