2c3c2db0dd975971d4e5503072101378cfeccabf
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-28 11:17:50 +0400

    adding delete action on motions grid

--- public/js/app/view/document/motions/Grid.js
+++ public/js/app/view/document/motions/Grid.js
@@ -11,10 +11,27 @@ Ext.define('Telasi.view.document.motions.Grid', {
     text: 'ადრესატი',
     dataIndex: 'name',
     width: 300,
+    menuDisabled: true,
+    sortable: false,
   }, {
     text: 'ტექსტი',
     dataIndex: 'motionText',
     flex: 1,
-    editor: { allowBlank: true }
-  }]
+    editor: { allowBlank: true },
+    menuDisabled: true,
+    sortable: false,
+  }, {
+    xtype: 'actioncolumn',
+    width: 30,
+    sortable: false,
+    menuDisabled: true,
+    items: [{
+      icon: '/images/delete.gif',
+      tooltip: 'ადრესატის წაშლა',
+      scope: this,
+      handler: function(grid, rowIndex) {
+        grid.getStore().removeAt(rowIndex);
+      }
+    }]
+  }],
 });
--- public/js/app/view/document/motions/GridController.js
+++
@@ -1,4 +0,0 @@
-Ext.define('Telasi.view.document.motions.GridController', {
-  extend : 'Ext.app.ViewController',
-  alias: 'controller.motionsGridController',
-});
