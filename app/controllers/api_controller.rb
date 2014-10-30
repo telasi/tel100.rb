dfbb0b004d90ef9a2877695063234daf44681305
Dimitri Kurashvili <dimakura@gmail.com>
2014-10-30 12:34:48 +0400

    getting current user and employee

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,8 @@
 # -*- encoding : utf-8 -*-
 class ApiController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  def current_user; @curruser ||= Sys::User.authenticate(params[:username], params[:password]) end
+  def current_empl; current_user.employee if current_user.present? end
+  def validate_login; raise 'not authorized' if current_user.blank? end
 end
