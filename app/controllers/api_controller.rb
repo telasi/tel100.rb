aa8fc6c49a7c608f54cf4a82b22cc7e4cd3a0c2c
Dimitri Kurashvili <dimakura@gmail.com>
2014-11-03 12:13:48 +0400

    render api exceptions

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,14 @@
 class ApiController < ActionController::Base
   protect_from_forgery with: :null_session
 
+  rescue_from Exception do |exception|
+    render_api_error exception.message
+  end
+
+  def render_api_error(error)
+    render json: { success: false, error: error.to_s }
+  end
+
   def current_user; @curruser ||= Sys::User.authenticate(params[:api_username], params[:api_password]) end
   def current_empl; current_user.employee if current_user.present? end
   def validate_login; raise 'not authorized' if current_user.blank? end
