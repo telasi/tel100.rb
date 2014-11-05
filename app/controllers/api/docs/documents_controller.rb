7235d52727c5fd6f2a9f2cc6dd56452d43f479b8
Dimitri Kurashvili <dimakura@gmail.com>
2014-11-05 17:52:17 +0400

    ordering motions

--- app/controllers/api/docs/documents_controller.rb
+++ app/controllers/api/docs/documents_controller.rb
@@ -42,16 +42,13 @@ class Api::Docs::DocumentsController < ApiController
 
   def motions
     if params[:flat]
-      render json: (Document::Motion.where(document_id: params[:id]).map { |x|
-        { id: x.id, motion_text: x.motion_text, due_date: x.due_date,
-          name: x.receiver_ext_name, receiver_id: x.receiver_ext_id, icon: x.receiver_ext_icon }
-      }.to_json)
+      render json: ( Document::Motion.where(document_id: params[:id]).map { |x| { id: x.id, motion_text: x.motion_text, due_date: x.due_date, name: x.receiver_ext_name, receiver_id: x.receiver_ext_id, icon: x.receiver_ext_icon } }.to_json )
     else
       render json: array_to_tree(Document::Motion.where(document_id: params[:id]).as_json)
     end
   end
 
   def authors
-    render json: ( Document::Author.where(document_id: params[:id]).order(:id).map { |x| { id: x.id, note: x.note,  name: x.author_ext_name, author_id: x.author_ext_id, icon: x.author_ext_icon } }.to_json)
+    render json: ( Document::Author.where(document_id: params[:id]).order(:id).map { |x| { id: x.id, note: x.note,  name: x.author_ext_name, author_id: x.author_ext_id, icon: x.author_ext_icon } }.to_json )
   end
 end
