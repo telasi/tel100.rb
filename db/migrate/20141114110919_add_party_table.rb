3cc7ca2a21f4e7a146705e02b31a448c537ff41a
temochutlashvili <temo.chutlashvili@telasi.ge>
2014-11-17 17:14:53 +0400

    add party tables

--- db/migrate/20141114110919_add_party_table.rb
+++ db/migrate/20141114110919_add_party_table.rb
@@ -1,40 +1,45 @@
 class AddPartyTable < ActiveRecord::Migration
   def up
     execute <<-SQL
-      create table PARTY (
-        ID          number(10, 0) not null,
-        ORG_ID		number(15, 0),
-        ORG_NAME	varchar2(200 CHAR),
-        PERSON_ID   number(15, 0),
-        PERSON_NAME	varchar2(200 CHAR),
-        ADDRESS		varchar2(500 CHAR),
-        PHONES		varchar2(1000 CHAR),
+      create table PARTY_BASE (
+        ID         number(10, 0) not null,
+        TYPE       varchar2(20 CHAR) not null,
+        IDENTITY	 varchar2(30 CHAR),
+        NAME_KA    varchar2(200 CHAR),
+        NAME_RU    varchar2(200 CHAR),
+        NAME_EN    varchar2(200 CHAR),
+        ADDRESS_KA varchar2(500 CHAR),
+        ADDRESS_RU varchar2(500 CHAR),
+        ADDRESS_EN varchar2(500 CHAR),
+        PHONES		 varchar2(100 CHAR),
+        EMAIL      varchar2(50 CHAR),
+        ACCOUNT    varchar2(50 CHAR),
         -----
         CREATED_AT TIMESTAMP WITH TIME ZONE default SYSTIMESTAMP not null,
         UPDATED_AT TIMESTAMP WITH TIME ZONE default SYSTIMESTAMP not null,
-        constraint PARTY_PRIMARYKEY primary key ( id ) enable
+        constraint PARTY_BASE_PRIMARYKEY primary key ( id ) enable
       )
     SQL
 
     execute <<-SQL
-      create sequence PARTY_SEQ increment by 1 start with 1 nocache
+      create sequence PARTY_BASE_SEQ increment by 1 start with 1 nocache
     SQL
 
     execute <<-SQL
-      create trigger PARTY_BEFORE_INSERT
-      before insert on PARTY
+      create trigger PARTY_BASE_BEFORE_INSERT
+      before insert on PARTY_BASE
       for each row
       BEGIN
         IF :new.ID is null
         THEN
-          :new.ID := PARTY_SEQ.nextval;
+          :new.ID := PARTY_BASE_SEQ.nextval;
         END IF;
       END;
     SQL
   end
 
   def down
-    execute "drop sequence PARTY_SEQ"
-  	execute "drop table PARTY"
+    execute "drop sequence PARTY_BASE_SEQ"
+  	execute "drop table PARTY_BASE"
   end
 end
---
+++ db/migrate/20141117105610_add_contacts_table.rb
@@ -0,0 +1,45 @@
+class AddContactsTable < ActiveRecord::Migration
+  def up
+	execute <<-SQL
+      create table PARTY_CONTACTS (
+        ID         		number(10, 0) not null,
+        PARENT_ID  		number(10, 0),
+        FIRST_NAME_KA 	varchar2(50 CHAR) not null,
+        FIRST_NAME_RU 	varchar2(50 CHAR),
+        FIRST_NAME_EN 	varchar2(50 CHAR),
+        LAST_NAME_KA  	varchar2(50 CHAR) not null,
+        LAST_NAME_RU  	varchar2(50 CHAR),
+        LAST_NAME_EN  	varchar2(50 CHAR),
+        PHONES			varchar2(20 CHAR),
+        EMAIL      		varchar2(50 CHAR),
+        ACCOUNT    		varchar2(50 CHAR),
+        -----
+        CREATED_AT TIMESTAMP WITH TIME ZONE default SYSTIMESTAMP not null,
+        UPDATED_AT TIMESTAMP WITH TIME ZONE default SYSTIMESTAMP not null,
+        constraint PARTY_CONTACTS_PRIMARYKEY primary key ( id ) enable
+      )
+    SQL
+
+    execute <<-SQL
+      create sequence PARTY_CONTACTS_SEQ increment by 1 start with 1 nocache
+    SQL
+
+    execute <<-SQL
+      create trigger PARTY_CON_BEFORE_INSERT
+      before insert on PARTY_CONTACTS
+      for each row
+      BEGIN
+        IF :new.ID is null
+        THEN
+          :new.ID := PARTY_CONTACTS_SEQ.nextval;
+        END IF;
+      END;
+    SQL
+  end
+
+  def down 
+    execute "drop sequence PARTY_CONTACTS_SEQ"
+  	execute "drop table PARTY_CONTACTS"
+  end
+
+end
