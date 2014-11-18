1369fd4f084bd1692c241a46e55ff95afba86d8d
Dimitri Kurashvili <dimakura@gmail.com>
2014-11-18 10:36:48 +0400

    adding not-sent status

--- app/models/document/status.rb
+++ app/models/document/status.rb
@@ -1,21 +1,30 @@
 # -*- encoding : utf-8 -*-
 module Document::Status
-  # signature / motion statuses
+  # Initial status.
   NONE     = DRAFT     =  0
+
+  # Sent status.
   PROCESS  = SENT      =  1
-  REJECTED = CANCELED  = -1
+
+  # Status for completed motion or signed document.
   SIGNED   = COMPLETED =  2
 
-  ALL_STATUSES = [ DRAFT, SENT, CANCELED, COMPLETED ]
+  # Sending document failed. Usually due to luck of 
+  NOT_SENT = -1
+
+  # Status for cancled motion or rejected signature.
+  REJECTED = CANCELED  = -2
+
+  ALL_STATUSES = [ DRAFT, SENT, COMPLETED, NOT_SENT, CANCELED ]
   OPEN_STATUSES = ALL_STATUSES[0..1]
   CLOSED_STATUSES = ALL_STATUSES - OPEN_STATUSES
 
-  def draft?; self.status == DRAFT end
-  def sent?; self.status == SENT end
-  def canceled?; self.canceled == CANCELED end
-  def completed?; self.completed == COMPLETED end
-  def open?; NEW_STATUSES.include? self.status end
-  def closed?; COMPLETED_STATUSES.include? self.status end
+  # def draft?; self.status == DRAFT end
+  # def sent?; self.status == SENT end
+  # def canceled?; self.canceled == CANCELED end
+  # def completed?; self.completed == COMPLETED end
+  # def open?; NEW_STATUSES.include? self.status end
+  # def closed?; COMPLETED_STATUSES.include? self.status end
 
   def self.included(base)
     base.extend(ClassMethods)
