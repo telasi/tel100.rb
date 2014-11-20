31841144d40aac8913bd2fd1b688b7ab7dfee7ea
Dimitri Kurashvili <dimakura@gmail.com>
2014-11-20 15:40:21 +0400

    fixing status store

--- public/js/app/store/document/Status.js
+++ public/js/app/store/document/Status.js
@@ -2,20 +2,25 @@ Ext.define('Telasi.store.document.Status', {
   extend: 'Ext.data.Store',
   fields: [ 'id', 'name', 'icon', 'class' ],
   data: [{
+    id: Telasi.statuses.not_sent,
+    name: 'გაუგზავნელი',
+    icon: 'fa-ban',
+    class: 'text-muted'
+  }, {
+    id: Telasi.statuses.canceled,
+    name: 'გაუქმებული',
+    icon: 'fa-times',
+    class: 'text-danger'
+  }, {
     id: Telasi.statuses.draft,
     name: 'დრაფტი',
     icon: 'fa-circle-o',
     class: 'text-muted'
   }, {
     id: Telasi.statuses.sent,
-    name: 'გაგზავნილი',
-    icon: 'fa-send-o',
+    name: 'მიმდინარე',
+    icon: 'fa-clock-o',
     class: 'text-primary'
-  }, {
-    id: Telasi.statuses.canceled,
-    name: 'გაუქმებული',
-    icon: 'fa-times',
-    class: 'text-danger'
   }, {
     id: Telasi.statuses.completed,
     name: 'შესრულებული',
