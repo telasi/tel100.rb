ab0ca8c7013efc15b287969809bed0dc0c024776
Dimitri Kurashvili <dimakura@gmail.com>
2014-11-28 14:04:00 +0400

    adding new?/changed? methods to motion/docuser

--- app/models/document/motion.rb
+++ app/models/document/motion.rb
@@ -7,10 +7,13 @@ class Document::Motion < ActiveRecord::Base
   include Document::Status
   self.table_name  = 'document_motion'
   self.sequence_name = 'docmotion_seq'
-  self.set_integer_columns :status, :is_read
+  self.set_integer_columns :status, :is_new
 
   belongs_to :document, class_name: 'Document::Base'
   belongs_to :parent, class_name: 'Document::Motion'
   personalize 'receiver'
   personalize 'sender'
+
+  def new=(val); self.is_new = val ? 1 : 0 end
+  def new?; self.is_new == 1 end
 end
--- app/models/document/user.rb
+++ app/models/document/user.rb
@@ -3,40 +3,35 @@ class Document::User < ActiveRecord::Base
   include Document::Role
   self.table_name  = 'document_user'
   self.primary_keys = :user_id, :document_id
-  self.set_integer_columns :is_read, :status
+  self.set_integer_columns :is_new, :is_changed, :status
   belongs_to :document, class_name: 'Document::Base', foreign_key: 'document_id'
   belongs_to :user, class_name: 'Sys::User', foreign_key: 'user_id'
   before_save :update_document_motions
 
   def self.upsert!(doc, user, role, opts={})
     if user.present?
-      params = { document_id: doc.id, user_id: user.id }
-      docuser = ( Document::User.where(params).first || Document::User.create!(params.merge(role: role)) )
+      params   = { document_id: doc.id, user_id: user.id }
+      docuser  = ( Document::User.where(params).first || Document::User.create!(params.merge(role: role)) )
       new_role = docuser.role
       new_role = role if Document::Role.compare(new_role, role) < 0
+      is_new   = opts[:is_new] || 1
+      is_changed = opts[:is_changed] || is_new
       docuser.update_attributes!({
         role: new_role,
         status: opts[:status] || doc.status,
-        is_read: opts[:is_read] || 0
+        is_new: is_new,
+        is_changed: is_changed
       })
     end
   end
 
-  def read!; self.update_is_read_attribute(1) end
-  def unread!; self.update_is_read_attribute(0) end
+  def motions; Document::Motion.where(document: self.document, receiver_user: self.user) end
+  def changed=(val); self.is_changed = val ? 1 : 0 end
+  def changed?; self.is_changed == 1 end
+  def new=(val); self.is_new = val ? 1 : 0 end
+  def new?; self.is_new == 1 end
 
   protected
 
-  def update_is_read_attribute(new_is_read)
-    Document::User.transaction do
-      self.update_columns(is_read: new_is_read)
-      update_document_motions
-    end
-  end
-
-  def update_document_motions
-    Document::Motion.where(document: self.document, receiver_user: self.user).each do |motion|
-      motion.update_columns(is_read: self.is_read)
-    end
-  end
+  def update_document_motions; motions.each { |motion| motion.update_attributes(is_new: self.is_new) } end
 end
