a2eb1d6d25dd0007857bcecfc7823c824cb698ab
Dimitri Kurashvili <dimakura@gmail.com>
2014-12-01 12:49:34 +0400

    change viewer layout

--- public/js/app/view/document/viewer/Detail.js
+++ public/js/app/view/document/viewer/Detail.js
@@ -22,9 +22,18 @@ Ext.define('Telasi.view.document.viewer.Detail', {
     listeners: { afterrender: function(){
       this.getForm().getFields().each(function(field){ field.setReadOnly(true) })
     }}
-  },{
+  }, {
+    xtype: 'panel',
+    title: '<i class="fa fa-edit"></i> ხელმომწერები',
+    padding: 0,
+    bodyPadding: 0
+  }, {
+    xtype: 'panel',
+    title: '<i class="fa fa-send-o"></i> ადრესატები',
+    bodyPadding: 0
+  }, {
     xtype: 'document-motiontree',
-    title: '<i class="fa fa-random"></i> დოკუმენტის მოძრაობა',
-    bodyPadding: 0,
+    title: '<i class="fa fa-car"></i> დოკუმენტის მოძრაობა',
+    bodyPadding: 0
   }],
 });
