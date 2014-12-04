51001064d64a97f9294267d8d1daa7a69ab29ec0
Dimitri Kurashvili <dimakura@gmail.com>
2014-12-04 18:20:29 +0400

    sending illegal username/password message

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -3,7 +3,7 @@ class Api::UserController < ApiController
   def login
     @user = Sys::User.authenticate(params[:userID], params[:password])
     if @user.blank?
-      render json: { success: false }
+      render json: { success: false, message: I18n.t('controllers.user.errors.illegal_username_and_password') }
     else
       render formats: ['json']
     end
