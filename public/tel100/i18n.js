a2be3bf61868029546a9c8c0621109aced4cf240
Dimitri Kurashvili <dimakura@gmail.com>
2014-12-04 14:17:06 +0400

    i18n.js resource

---
+++ public/tel100/i18n.js
@@ -0,0 +1,19 @@
+var i18n = {};
+i18n.ka  = {};
+i18n.ru  = {};
+
+i18n.ka.user = {
+  login:     'სისტემაში შესვლა',
+  login_btn: 'შესვლა &rarr;',
+  username:  'მომხ. სახელი',
+  password:  'პაროლი',
+  language:  'ინტერფეისის ენა'
+};
+
+i18n.ru.user = {
+  login:     'Вход в систему',
+  login_btn: 'Вход &rarr;',
+  username:  'Пользователь',
+  password:  'Пароль',
+  language:  'Язык интерфейса'
+};
