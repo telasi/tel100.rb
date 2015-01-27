d98b26c06e99569e5f12801ad92897fc2a45dfbd
Dimitri Kurashvili <dimakura@gmail.com>
2015-01-27 10:16:30 +0400

    more advanced version of Document::Motion#create_draft! method

--- app/models/document/motion.rb
+++ app/models/document/motion.rb
@@ -21,19 +21,24 @@ class Document::Motion < ActiveRecord::Base
   def can_destroy?; self.new? end
 
   def self.create_draft!(sender_user, params)
-    document = Document::Base.find(params[:document_id])
-    parent = Document::Motion.find(params[:parent_id]) if params[:parent_id]
-
+    document_id = params[:document_id] ; parent_id = params[:parent_id]
+    document = Document::Base.find(document_id)
+    parent = Document::Motion.find(parent_id) if parent_id.present?
+    # check user permission for this action
+    is_receiver_user = ( parent.present? and sender_user == parent.receiver_user )
+    is_owner_user = ( parent.blank? and sender_user == document.owner_user )
+    raise "#{sender_user.username} doesn't have permission for this action" unless is_receiver_user or is_owner_user
+    # sender/receiver information
     sender = whose_user(sender_user)
     receiver_user, receiver = who_eval('receiver', params)
-
-    same_receiver_user = receiver_user.present? and sender_user == receiver_user
-    same_receiver = receiver.present? and sender.present? and sender == receiver
+    same_receiver_user = ( receiver_user.present? and sender_user == receiver_user )
+    same_receiver = ( receiver.present? and sender.present? and sender == receiver )
     raise 'sender should not send to himself' if same_receiver or same_receiver_user
-
-    # TODO: check permission
-    # TODO: check existence of this receiver on the branch
-
+    # check existence of this receiver on the branch
+    receiver_count = receiver.present? ? document.motions.where(parent_id: parent_id, receiver: receiver).count : 0
+    receiver_user_count = receiver_user.present? ? document.motions.where(parent_id: parent_id, receiver_user: receiver_user).count : 0
+    raise 'this receiver is already on this branch' if ( receiver_count > 0 or receiver_user_count > 0 )
+    # create this
     Document::Motion.create!({
       parent: parent,
       document: document,
