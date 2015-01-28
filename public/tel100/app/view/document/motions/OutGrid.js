18bd818f7793a43c19db2bc2545889c226b97877
Dimitri Kurashvili <dimakura@gmail.com>
2015-01-28 15:36:41 +0400

    receiver rendering in motion panel

--- public/tel100/app/view/document/motions/OutGrid.js
+++ public/tel100/app/view/document/motions/OutGrid.js
@@ -63,6 +63,14 @@ Ext.define('Tel100.view.document.motions.OutGrid', {
     },
     {
       xtype: 'gridcolumn',
+      renderer: function(value, metaData, record, rowIndex, colIndex, store, view) {
+        var receiver = record.get('receiver');
+        if (receiver && receiver.ext_type == 'hr.Organization') {
+          return '<i class="fa fa-bank"></i> ' + value;
+        } else {
+          return '<i class="fa fa-user"></i> ' + value;
+        }
+      },
       sortable: false,
       dataIndex: 'receiverName',
       hideable: false,
