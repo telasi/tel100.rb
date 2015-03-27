4888ac3aaa492c5a51a661b8957c19f1deb79a52
Dimitri Kurashvili <dimakura@gmail.com>
2015-03-27 11:14:50 +0400

    don't allow duplicate relations

--- app/controllers/api/documents/relations_controller.rb
+++ app/controllers/api/documents/relations_controller.rb
@@ -7,6 +7,8 @@ class Api::Documents::RelationsController < ApiController
   def create
     document = Document::Base.find(params[:base_id])
     related = Document::Base.find(params[:related_id])
+    rel = Document::Relation.where(base: document, related: related).first
+    raise I18n.t('models.document_relations.errors.relation_exists') if rel.present?
     rel = Document::Relation.create(base: document, related: related)
     render json: { id: rel.id }
   end
