77f6a1990c086523fcb75d0ce6777008a48fe37c
T <opit@gol.ge>
2015-03-31 10:02:09 +0400

    folders: sent documents

--- app/models/folder/standard.rb
+++ app/models/folder/standard.rb
@@ -58,10 +58,9 @@ class Folder::Standard
    		when INBOX_READ
    			Document::User.where('document_user.status = ? AND is_new = ? AND user_id = ?', Document::Status::CURRENT, 0, user.id)
    		when INBOX_RESENT
-        Document::Motion.where('document_motion.parent_id IS NOT NULL and document_motion.sender_user_id = ? ', user.id)
+        Document::User.where('document_user.status = ? AND user_id = ?', Document::Status::CURRENT, 0, user.id)
    		when SENT
-   			Document::User.joins('JOIN document_motion on document_motion.document_id = document_user.document_id')
-                      .where('document_motion.status = ? AND document_motion.sender_user_id = ?', Document::Status::SENT, user.id)
+   			Document::User.where("document_user.role = 'owner' and document_user.status <> ? AND document_user.user_id = ?", Document::Status::DRAFT, user.id)
       when COMPLETED
         Document::User.where('document_user.status = ? AND user_id = ?', Document::Status::COMPLETED, user.id)
       when CANCELED
