6fb9a25d4b7d08d13e1c98a1d0525155358e99d3
Dimitri Kurashvili <dimakura@gmail.com>
2015-04-07 12:33:36 +0400

    extend Document::ResponseType class

--- app/models/document/response_type.rb
+++ app/models/document/response_type.rb
@@ -1,8 +1,8 @@
 # -*- encoding : utf-8 -*-
 class Document::ResponseType < ActiveRecord::Base
-  SEND     = 'snd'
-  COMPLETE = 'pos'
-  CANCEL   = 'neg'
+  SEND          = 1
+  RESP_COMPLETE = 2
+  RESP_CANCEL   = 3
 
   self.table_name  = 'DOCUMENT_RESPONSE_TYPES'
   self.sequence_name = 'DOCRESPTYPE_SEQ'
@@ -10,4 +10,20 @@ class Document::ResponseType < ActiveRecord::Base
 
   belongs_to :document, class_name: 'Document::Base', foreign_key: 'document_id'
   belongs_to :user, class_name: 'Sys::User', foreign_key: 'user_id'
+
+  def positive?
+    self.direction == RESP_COMPLETE
+  end
+
+  def negative?
+    self.direction == RESP_CANCEL
+  end
+
+  def self.send_types
+    Document::ResponseType.where('direction IN (?)', [ SEND ])
+  end
+
+  def self.response_types
+    Document::ResponseType.where('direction IN (?)', [ RESP_COMPLETE, RESP_CANCEL ])
+  end
 end
