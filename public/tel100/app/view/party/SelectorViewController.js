277a25729642e3f035c73f8403573096fffa484d
T <opit@gol.ge>
2015-04-15 16:36:35 +0400

    favourites

--- public/tel100/app/view/party/SelectorViewController.js
+++ public/tel100/app/view/party/SelectorViewController.js
@@ -26,6 +26,71 @@ Ext.define('Tel100.view.party.SelectorViewController', {
     var grid = this.getView().getComponent('selectedParties');
     var store = grid.getStore();
     store.remove(obj);
+  },
+
+  contextMenu: function(dataview, record, item, index, e) {
+    var me = this;
+    var favMenu = Ext.create('Ext.menu.Menu', {
+        items: [{
+          text: i18n.hr.favourites.add_to,
+          icon: '/images/add.png',
+          handler: function(item){
+            var model = Ext.create('Tel100.model.party.Favourites',
+                                    { person_id: record.id, person_type: record.get('ext_type') });
+            model.save();
+          }
+        }]
+      });
+
+      e.stopEvent();
+      favMenu.showAt(e.getXY());
+  },
+
+  onFavouritesBeforeItemContextMenu: function(dataview, record, item, index, e, eOpts) {
+    var rec = record;
+    var favMenu = Ext.create('Ext.menu.Menu', {
+      items: [{
+        text: i18n.hr.favourites.delete,
+        icon: '/images/delete.png',
+        handler: function(item){
+          debugger;
+          var store = grid.store;
+          var selection = grid.selection;
+          if (selection){
+            Ext.Msg.show({
+              title: i18n.ui.destroy,
+              message: i18n.ui.destroyConfirm,
+              buttons: Ext.Msg.YESNO,
+              icon: Ext.Msg.QUESTION,
+              fn: function(btn) {
+                if (btn === 'yes') {
+                  store.remove(selection);
+                  store.sync();
+                  store.reload();
+                }
+              }
+            });
+          }
+        }
+      }]
+    });
+
+    e.stopEvent();
+    favMenu.showAt(e.getXY());
+  },
+
+  onHRStructureBeforeItemContextMenu: function(dataview, record, item, index, e, eOpts) {
+    if(record.get('ext_type') == 'hr.Employee'){
+      this.contextMenu(dataview, record, item, index, e);
+    }
+  },
+
+  onPartiesBeforeItemContextMenu: function(dataview, record, item, index, e, eOpts) {
+    this.contextMenu(dataview, record, item, index, e);
+  },
+
+  onCustomerBeforeItemContextMenu: function(dataview, record, item, index, e, eOpts) {
+    this.contextMenu(dataview, record, item, index, e);
   }
 
 });
