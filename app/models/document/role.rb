5fcdecbe5174afde441209bfd9ba451dba25927c
Dimitri Kurashvili <dimakura@gmail.com>
2015-05-06 10:49:13 +0400

    adding sender role

--- app/models/document/role.rb
+++ app/models/document/role.rb
@@ -1,11 +1,8 @@
 # -*- encoding : utf-8 -*-
 module Document::Role
-  # მფლობელი/ინიციატორი
   ROLE_OWNER    = 'owner'
-  # ავტორი
+  ROLE_SENDER   = 'sender'
   ROLE_AUTHOR   = 'author'
-  # ვიზატორი
   ROLE_SIGNEE   = 'signee'
-  # შემსრულებელი
   ROLE_ASSIGNEE = 'assignee'
 end
