7c8c4fc6ed57c82eb4ad54b725b7bd05e4eb246d
Dimitri Kurashvili <dimakura@gmail.com>
2015-05-15 11:57:37 +0400

    logout functionality

--- public/tel100/app/view/user/box/ButtonViewController.js
+++ public/tel100/app/view/user/box/ButtonViewController.js
@@ -3,7 +3,14 @@ Ext.define('Tel100.view.user.box.ButtonViewController', {
   alias: 'controller.userboxbutton',
 
   onLogout: function(item, e, eOpts) {
-    window.location.reload();
+    var proxyUser = helpers.user.getProxyUser();
+    if (proxyUser) {
+      var view = this.getView();
+      helpers.user.setProxyUser(null);
+      view.up('main').getViewModel().set('proxyUser', null);
+    } else {
+      window.location.reload();
+    }
   },
 
   onSwitch: function(item, e, eOpts) {
