58aea9b7342195b4ef455a1799a48eeac8f1bc35
Dimitri Kurashvili <dimakura@gmail.com>
2015-05-18 15:49:41 +0400

    sync

--- public/tel100/app/view/document/Main.js
+++ public/tel100/app/view/document/Main.js
@@ -9,85 +9,72 @@ Ext.define('Tel100.view.document.Main', {
   layout: 'border',
   defaultListenerScope: true,
 
-  items: [
-    {
-      xtype: 'documentfoldertab',
-      width: 250,
-      collapsible: true,
-      region: 'west',
-      split: true,
-      listeners: {
-        folderChosen: {
-          fn: 'onTabpanelFolderChosen',
-          scope: 'controller'
-        },
-        foldersrefresh: {
-          fn: 'onFoldersRefresh',
-          scope: 'controller'
-        }
+  items: [{
+    xtype: 'documentfoldertab',
+    width: 250,
+    collapsible: true,
+    region: 'west',
+    split: true,
+    listeners: {
+      folderChosen: {
+        fn: 'onTabpanelFolderChosen',
+        scope: 'controller'
+      },
+      foldersrefresh: {
+        fn: 'onFoldersRefresh',
+        scope: 'controller'
       }
-    },
-    {
-      xtype: 'tabpanel',
-      region: 'center',
-      border: false,
-      itemId: 'documentTabs',
-      activeTab: 0,
-      items: [
-        {
-          xtype: 'panel',
-          dockedItems: [
-            {
-              xtype: 'toolbar',
-              dock: 'top',
-              items: [
-                {
-                  handler: 'onRefresh',
-                  bind: {
-                    text: '{i18n.document.base.ui.refresh}'
-                  }
-                },
-                {
-                  handler: 'onNewDocument',
-                  cls: 'success-button',
-                  bind: {
-                    text: '{i18n.document.base.ui.newDocument}'
-                  }
-                },
-                '->',
-                {
-                  handler: 'onDeleteDraft',
-                  cls: 'danger-button',
-                  bind: {
-                    text: '{i18n.document.base.ui.deleteDraft}',
-                    disabled: '{deleteDraftButtonDisabled}'
-                  }
-                }
-              ]
-            }
-          ],
-          border: false,
-          layout: 'fit',
-          bind: {
-            title: '{i18n.document.base.ui.documents}'
-          },
-          items: [
-            {
-              xtype: 'documentgridpanel',
-              bind: {
-                selection: '{selection}'
-              },
-              listeners: {
-                documentopen: 'onGridpanelDocumentopen',
-                documentsign: 'onGridpanelDocumentsign',
-                documentauthor: 'onGridpanelDocumentauthor'
-              }
-            }
-          ]
-        }
-      ]
     }
-  ],
+  }, {
+    xtype: 'tabpanel',
+    region: 'center',
+    border: false,
+    itemId: 'documentTabs',
+    activeTab: 0,
+    items: [{
+      xtype: 'panel',
+      dockedItems: [{
+        xtype: 'toolbar',
+        dock: 'top',
+        items: [{
+          handler: 'onRefresh',
+          bind: {
+            text: '{i18n.document.base.ui.refresh}'
+          }
+        }, {
+          handler: 'onNewDocument',
+          cls: 'success-button',
+          bind: {
+            text: '{i18n.document.base.ui.newDocument}'
+          }
+        }, '->', {
+          handler: 'onDeleteDraft',
+          cls: 'danger-button',
+          bind: {
+            text: '{i18n.document.base.ui.deleteDraft}',
+            disabled: '{deleteDraftButtonDisabled}'
+          }
+        }]
+      }],
+      border: false,
+      layout: 'fit',
+      bind: {
+        title: '{i18n.document.base.ui.documents}'
+      },
+      items: [{
+        xtype: 'documentgridpanel',
+        bind: {
+          selection: '{selection}'
+        },
+        listeners: {
+          documentopen: 'onGridpanelDocumentopen',
+          documentsign: 'onGridpanelDocumentsign',
+          documentauthor: 'onGridpanelDocumentauthor'
+        }
+      }]
+    }]
+  }],
+
   listeners: {
     beforerender: 'onPanelBeforeRender'
   },
