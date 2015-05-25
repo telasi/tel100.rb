a9a6a71794744c80647bc9d5c8c7a579bd9c0500
Dimitri Kurashvili <dimakura@gmail.com>
2015-05-25 13:00:43 +0400

    define inner/outer party methods

--- app/models/document/direction.rb
+++ app/models/document/direction.rb
@@ -3,4 +3,16 @@ module Document::Direction
   INNER = 'inner'
   IN = 'in'
   OUT = 'out'
+
+  def inner_party?(party)
+    if party.present?
+      return ['Sys::User', 'HR::Employee', 'HR::Organization'].include?(party.class.name)
+    end
+  end
+
+  def outer_party?(party)
+    if party.present?
+      not self.inner_party?(party)
+    end
+  end
 end
