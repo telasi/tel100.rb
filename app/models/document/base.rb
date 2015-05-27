c7d50f8e0a02b376e49905d361ff1a884cadfeed
Dimitri Kurashvili <dimakura@gmail.com>
2015-05-27 12:09:01 +0400

    fixign auto-assignee procedure

--- app/models/document/base.rb
+++ app/models/document/base.rb
@@ -118,15 +118,15 @@ class Document::Base < ActiveRecord::Base
     # ignore inner documents
     return if self.direction == INNER
     # calculate auto assingees
-    receiver_ids = self.motions.where(status: RECEIVED).to_a.map{ |motion| motion.receiver_user }.flatten.map{|user| user.id}.uniq
+    receiver_ids = self.motions.where(status: CURRENT).to_a.map{ |motion| motion.receiver_user }.flatten.map{|user| user.id}.uniq
     auto_assignee_ids = Sys::UserRelation.where(role: ROLE_AUTO_ASSIGNEE).where('USER_ID IN (?)', receiver_ids).to_a.map{ |rel| rel.related_id }.uniq
     # process each auto-assignee
     send_type_direction = Document::ResponseTypeDirection::SEND
     send_type = Document::ResponseType.where(direction: send_type_direction, role: ROLE_AUTO_ASSIGNEE).first
     auto_assignee_ids.each do |auto_assignee_id|
       unless self.motions.where(receiver_user_id: auto_assignee_id).any?
-        motion = Document::Motion.create!(document: self, parent: motion, is_new: 1, ordering: 1, send_type: send_type,
-          sender_user: user, receiver_user_id: auto_assignee_id, receiver_role: ASSIGNEE, status: CURRENT,
+        motion = Document::Motion.create!(document: self, parent: motion, is_new: 1, ordering: Document::Motion::MAX,
+          send_type: send_type, sender_user: user, receiver_user_id: auto_assignee_id, receiver_role: ROLE_ASSIGNEE, status: CURRENT,
           sent_at: Time.now, received_at: Time.now)
         docuser = Document::User.create!(document: self, user_id: auto_assignee_id, is_new: 1, is_changed: 1,
           is_shown: 1, is_received: 1, is_current: 1, as_assignee: DOC_CURRENT)
