6914ccfc49a14c6f65f578dee79f06a364363f85
Dimitri Kurashvili <dimakura@gmail.com>
2015-05-27 10:49:08 +0400

    adding new role to response type editor

--- app/helpers/response_types_helper.rb
+++ app/helpers/response_types_helper.rb
@@ -2,11 +2,22 @@
 module ResponseTypesHelper
   include Document::ResponseTypeDirection
 
-  def response_type_directions()
+  def response_type_directions
     {
       SEND => Document::ResponseType.direction_name(SEND),
       RESP_COMPLETE => Document::ResponseType.direction_name(RESP_COMPLETE),
       RESP_CANCEL => Document::ResponseType.direction_name(RESP_CANCEL)
+    }
+  end
+
+  def response_type_roles
+    {
+      ROLE_OWNER => ROLE_OWNER,
+      ROLE_SENDER => ROLE_SENDER,
+      ROLE_AUTHOR => ROLE_AUTHOR,
+      ROLE_SIGNEE => ROLE_SIGNEE,
+      ROLE_ASSIGNEE => ROLE_ASSIGNEE,
+      ROLE_AUTO_ASSIGNEE => ROLE_AUTO_ASSIGNEE
     }
   end
 end
