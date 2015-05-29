04e135686b44646507f71708a8e2d84973a28b96
Dimitri Kurashvili <dimakura@gmail.com>
2015-05-29 12:58:57 +0400

    adding Eflow::Document model

---
+++ app/models/eflow/document.rb
@@ -0,0 +1,5 @@
+class Eflow::Document < ActiveRecord::Base
+  establish_connection 'eflow'
+  self.table_name = 'eflow.docs_documents'
+  self.primary_key = 'document_id'
+end
--- app/models/eflow/motion.rb
+++ app/models/eflow/motion.rb
@@ -1,5 +1,6 @@
 class Eflow::Motion < ActiveRecord::Base
   establish_connection 'eflow'
-  self.table_name = 'eflow.docs_motion'
+  self.table_name = 'eflow.docs_motions'
   self.primary_key = 'motion_id'
+  belongs_to :document, class_name: 'Eflow::Document', foreign_key: 'document_id'
 end
