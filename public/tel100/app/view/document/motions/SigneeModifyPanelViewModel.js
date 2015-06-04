8396ef149c5e730f49c145ffb459628662a7cafb
T <opit@gol.ge>
2015-06-04 16:55:52 +0400

    signee modify panel

---
+++ public/tel100/app/view/document/motions/SigneeModifyPanelViewModel.js
@@ -0,0 +1,38 @@
+Ext.define('Tel100.view.document.motions.SigneeModifyPanelViewModel', {
+  extend: 'Ext.app.ViewModel',
+  alias: 'viewmodel.documentmotionssigneemodifypanel',
+
+  data: {
+    assigneeCount: 0,
+    selection: null
+  },
+
+  stores: {
+    motions: {
+      // onStoreChanges: function() {
+      //   var store = this;
+      //   this.viewModel.set('assigneeCount', store.getCount());
+      //   this.view.fireEvent('listchanged', store);
+      // },
+      // listeners: {
+      //   datachanged: function() {
+      //     this.onStoreChanges();
+      //   }
+      // },
+      autoLoad: true,
+      model: 'Tel100.model.document.Motion',
+      proxy: {
+        type: 'ajax',
+        extraParams: {
+          mode: 'out',
+          role: 'signee',
+          document_id: '{document.id}'
+        },
+        url: '/api/documents/motion',
+        reader: {
+          type: 'json'
+        }
+      }
+    }
+  }
+});
