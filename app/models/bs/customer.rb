db6db19d6d2a4b9ee6e50f8df53b748cbbf3eaee
Dimitri Kurashvili <dimakura@gmail.com>
2015-06-08 10:55:48 +0400

    customer HTML representation

--- app/models/bs/customer.rb
+++ app/models/bs/customer.rb
@@ -4,4 +4,8 @@ class BS::Customer < ActiveRecord::Base
   self.primary_key = 'custkey'
 
   def to_s; self.name end
+
+  def to_html
+    "<code>#{self.accnumb}</code> #{self.name} (მიმდ.ბალანსი: <code>#{self.balance}</code>GEL)"
+  end
 end
