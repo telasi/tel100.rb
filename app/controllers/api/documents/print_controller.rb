439be7d9b7f88a61a1ecbcffa11039a210677e30
Dimitri Kurashvili <dimakura@gmail.com>
2015-06-11 13:50:53 +0400

    new printing page

--- app/controllers/api/documents/print_controller.rb
+++ app/controllers/api/documents/print_controller.rb
@@ -1,13 +1,16 @@
 # -*- encoding : utf-8 -*-
 class Api::Documents::PrintController < ApiController
-
   def card
   	I18n.locale = params[:lang]
     @document = Document::Base.find(params[:id])
   end
 
   def print
-  	I18n.locale = params[:lang]
-  	@document = Document::Base.find(params[:id])
+    @assignees = params[:assignees] == 'true'
+    @author = params[:author] == 'true'
+    @signature = params[:signature] == 'true'
+    @subject = params[:subject] == 'true'
+    I18n.locale = params[:lang]
+    @document = Document::Base.find(params[:id])
   end
 end
