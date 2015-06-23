7af4b2b39e83fb05b77c818b326e771677ab7e67
Dimitri Kurashvili <dimakura@gmail.com>
2015-06-23 14:52:49 +0400

    cleanup document.editor.Editor

--- public/tel100/app/view/document/editor/Editor.js
+++ public/tel100/app/view/document/editor/Editor.js
@@ -327,5 +327,234 @@ Ext.define('Tel100.view.document.editor.Editor', {
     });
     historyWindow.show();
   }
+});
 
+// EditorViewController
+
+
+Ext.define('Tel100.view.document.editor.EditorViewController', {
+  extend: 'Ext.app.ViewController',
+  alias: 'controller.documenteditoreditor',
+
+  onEditClick: function(button, e, eOpts) {
+    var vm = this.getViewModel();
+    var document = vm.get('document');
+    var title = [ i18n.document.comment.actions.edit, ': ', document.get('docnumber')].join('');
+
+    var editWindow = Ext.create('Tel100.view.document.editor.Modify', {
+      title: title,
+      viewModel: {
+        data: {
+          document: document
+        }
+      }
+    });
+
+    editWindow.show();
+  },
+
+  onReplyClick: function(button, e, eOpts) {
+    var vm = this.getViewModel();
+    var document = vm.get('document');
+
+    helpers.api.document.base.reply(document.id, {
+      success: function(data) {
+        var doc = Ext.create('Tel100.model.document.Base', data);
+        var dm = this.getView().up('documentmain');
+        dm.getViewModel().set('selection', doc);
+        dm.getController().openDocument(doc);
+      }.bind(this)
+    });
+  },
+
+  onCardPrintClick: function(button, e, eOpts) {
+    var vm = this.getViewModel();
+    var document = vm.get('document');
+    var url = '/api/documents/print/card/' + document.id + '?lang=' + helpers.i18n.getCurrentLocale(); 
+    helpers.api.document.print.showPDFwindow(url);
+  },
+
+  onDocumentPrint: function(printParams) {
+    var vm = this.getViewModel();
+    var document = vm.get('document');
+    var url = '/api/documents/print/document/' + document.id + '?lang=' + helpers.i18n.getCurrentLocale();
+    for(var key in printParams){
+      if(printParams[key]){
+        url += '&'+ key + '=true';
+      }
+    }
+    // helpers.api.document.print.showPDFwindow(url);
+    window.open(url, 'tel100');
+  },
+
+  onInMotionChanged: function(motion) {
+    var view = this.getView();
+    var outPanel = view.down('documentmotionsoutpanel');
+    if (!motion || motion.get('type') === 'document') {
+      outPanel.setParentId(null);
+    } else {
+      outPanel.setParentId(motion.id);
+    }
+    outPanel.refresh();
+  },
+
+  onDestroy: function(component, eOpts) {
+    if (component.commentsDialog) {
+      component.commentsDialog.destroy();
+    }
+  },
+
+  onContainerAfterRender: function(component, eOpts) {
+    helpers.party.employeeTips(component);
+  },
+
+  onMenucheckitemCheckChange: function(menucheckitem, checked, eOpts){
+    var vm = this.getViewModel();
+    var field = 'printParams.' + menucheckitem.itemId;
+    vm.set(field, checked);
+  }
+});
+
+// EditorViewModel
+
+Ext.define('Tel100.view.document.editor.EditorViewModel', {
+  extend: 'Ext.app.ViewModel',
+  alias: 'viewmodel.documenteditoreditor',
+
+  data: {
+    document: null,
+    printParams: {
+      subject: false,
+      signature: false,
+      assignees: false,
+      author: false
+    }
+  },
+
+  formulas: {
+    hideEditButton: function(get){
+      return !get('document.is_editable');
+    },
+
+    hideSignButton: function(get) {
+      return get('document.as_signee') !== 1;
+    },
+
+    hideAuthorButton: function(get) {
+      return get('document.as_author') !== 1;
+    },
+
+    hideHistoryButton: function(get) {
+      return !get('document.has_history');
+    },
+
+    authors: function(get) {
+      var authors = get('document.authors');
+      var text = [];
+      if (authors.length) {
+        for(var i = 0; i < authors.length; i++) {
+          var author = authors[i];
+          var decor = helpers.document.status.statusDecoration(author.status);
+          text.push([
+          '<span class="' + decor.style + '">',
+          '<i class="fa ' + decor.icon + '"></i> ',
+          '<a data-id="' + author.author_id + '" data-class="' + author.author_type + '">' + author.name + '</a>',
+          ( author.response ? ' &mdash; ' + author.response : '' ),
+          '</span>'
+          ].join(''));
+        }
+        return text.join('; ');
+      } else {
+        var status = get('document.status');
+        var decor = helpers.document.status.statusDecoration(status);
+        var senderName = get('document.sender_name');
+        var senderId = get('document.sender_id');
+        var senderType = get('document.sender_type');
+        return [
+        //     '<strong class="text-success">' + i18n.document.base.sender + '</strong> &mdash; ',
+        '<span class="' + decor.style + '">',
+        '<i class="fa ' + decor.icon + '"></i> ',
+        '<a data-id="' + senderId + '" data-class="' + senderType + '">' + senderName + '</a>',
+        '</span>'
+        ].join('');
+      }
+    },
+
+    hideSignees: function(get) {
+      var signees = get('document.signees');
+      return !signees || signees.length === 0;
+    },
+
+    signees: function(get) {
+      var signees = get('document.signees');
+      var text = [];
+      for(var i = 0; i < signees.length; i++) {
+        var signee = signees[i];
+        var decor = helpers.document.status.statusDecoration(signee.status);
+        text.push([
+        '<span class="' + decor.style + '">',
+        '<i class="fa ' + decor.icon + '"></i> ',
+        '<a data-id="' + signee.signee_id + '" data-class="' + signee.signee_type + '">' + signee.name + '</a>',
+        ( signee.response ? ' &mdash; ' + signee.response : '' ),
+        '</span> ',
+        ( signee.completed_at ? '<span class="text-danger">' + signee.completed_at + '</span>' : '' )
+        ].join(''));
+      }
+      return text.join('; ');
+    },
+
+    docinfo: function(get) {
+      var docnumber = get('document.docnumber');
+      var typeName = get('document.type.name');
+      var status = get('document.status');
+      var decor = helpers.document.status.statusDecoration(status);
+
+      var actualSender = get('document.actual_sender');
+      var senderName = get('document.sender_name');
+      var senderId = get('document.sender_id');
+      var senderUserId = get('document.sender_user_id');
+      var senderType = get('document.sender_type');
+      var sentAt = get('document.sent_at_f');
+      var senderText = '<strong><a data-id="' + senderId + '" data-class="' + senderType + '">' + senderName + '</a></strong>';
+      if (actualSender && senderUserId !== actualSender.id) {
+        senderText += ' (' + '<strong><a data-id="' + actualSender.id + '" data-class="Sys::User">' + actualSender.name + '</a>)';
+      }
+
+      return [
+        '<span class="' + decor.style + '">',
+        '<i class="fa ' + decor.icon + '"></i> ',
+        '<strong>' + docnumber + '</strong>',
+        '</span> ',
+        '<span class="text-muted">' + typeName + '</span> &mdash; ',
+        senderText + ', ',
+        '<span class="text-danger">' + sentAt + '</span>'
+      ].join('');
+    },
+
+    incoming: function(get) {
+      var incoming = get('document.incoming');
+      var text = [];
+      incoming.forEach(function(motion) {
+        text.push(helpers.document.renderer.renderMotion(motion, { as: 'sender' }));
+      });
+      return text.join('; ');
+    },
+
+    hideIncoming: function(get) {
+      return !get('document.incoming').length;
+    },
+
+    outgoing: function(get) {
+      var outgoing = get('document.outgoing');
+      var text = [];
+      outgoing.forEach(function(motion) {
+        text.push(helpers.document.renderer.renderMotion(motion, { as: 'receiver' }));
+      });
+      return text.join('; ');
+    },
+
+    hideOutgoing: function(get) {
+      return !get('document.outgoing').length;
+    }
+  }
 });
