2d207be75694f1b4c096706528ee540d7aa4efcb
Dimitri Kurashvili <dimakura@gmail.com>
2015-07-07 18:17:18 +0400

    returning to plain html

--- app/controllers/api/documents/print_controller.rb
+++ app/controllers/api/documents/print_controller.rb
@@ -15,29 +15,6 @@ class Api::Documents::PrintController < ApiController
     @subject = params[:subject] == 'true'
     I18n.locale = params[:lang]
     @document = Document::Base.find(params[:id])
-    html = render_to_string "api/documents/print/print"
-    margins = @document.type.margins
-    if @document.type.print_header?
-      number = @document.docnumber ? @document.docnumber : '--'
-      date = @document.docdate ? @document.docdate.strftime('%d/%m/%Y')  : '--'
-    end
-    file = print_html I18n.locale, html, margins, number, date
-    send_file file, type: 'application/pdf', disposition: 'inline'
-  end
-
-  private
-
-  def print_html(locale, html, margins, number, date)
-    src = Tempfile.new('html')
-    src.write(html)
-    src.close
-    dest = Tempfile.new('pdf')
-
-    cmd = "java -cp lib/tel100.jar:lib/xmlworker.jar:lib/itextpdf.jar:lib/commons-io.jar:lib/jsoup.jar tel100.HTMLToPdf #{I18n.locale} #{src.path} #{dest.path} #{margins.join(' ')}"
-    if number.present?
-      cmd = "#{cmd} #{number} #{date}"
-    end
-    `#{cmd}`
-    dest
+    @margins = @document.type.margins
   end
 end
