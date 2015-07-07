45e0c84fa151bb4d6862e993b1761c9654a9f5a7
T <opit@gol.ge>
2015-07-07 12:44:59 +0400

    grid dblclick on signee now independent of column position

--- public/tel100/app/view/document/grid/Panel.js
+++ public/tel100/app/view/document/grid/Panel.js
@@ -224,8 +224,10 @@ Ext.define('Tel100.view.document.grid.Panel', {
   },
 
   onGridpanelCellDblClick: function(tableview, td, cellIndex, record, tr, rowIndex, e, eOpts) {
+    var cell = tableview.panel.columns[cellIndex];
     var actionIndex = 1; // index of sign column
-    if (cellIndex === actionIndex && record.get('as_signee') === 1) {
+    //if (cellIndex === actionIndex && record.get('as_signee') === 1) {
+    if (cell.dataIndex === 'statusName' && record.get('as_signee') === 1) {
       this.fireEvent('documentsign', record);
     } else if (cellIndex === actionIndex && record.get('as_author') === 1) {
       this.fireEvent('documentauthor', record);
