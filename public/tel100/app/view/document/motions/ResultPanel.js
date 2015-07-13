d640822bd2d199dee383c965f607283b63213858
Dimitri Kurashvili <dimakura@gmail.com>
2015-07-13 17:44:37 +0400

    change resultPanel

--- public/tel100/app/view/document/motions/ResultPanel.js
+++ public/tel100/app/view/document/motions/ResultPanel.js
@@ -90,3 +90,145 @@ Ext.define('Tel100.view.document.motions.ResultPanel', {
     completeCheck.setValue(false);
   }
 });
+
+Ext.define('Tel100.view.document.motions.ResultPanelViewModel', {
+  extend: 'Ext.app.ViewModel',
+  alias: 'viewmodel.documentmotionsresultpanel',
+
+  data: {
+    motionId: null,
+    isResult: false,
+    categoryId: null,
+    text: null,
+    selection: null
+  },
+
+  stores: {
+    motions: {
+      autoLoad: true,
+      model: 'Tel100.model.document.Motion',
+      proxy: {
+        type: 'ajax',
+        extraParams: {
+          mode: 'in',
+          document_id: '{document.id}'
+        },
+        url: '/api/documents/motion',
+        reader: {
+          type: 'json'
+        }
+      },
+      listeners: {
+        load: 'onMotionsStoreLoad'
+      }
+    },
+
+    responseTypes: {
+      autoLoad: true,
+      model: 'Tel100.model.document.ResponseType',
+      filters: {
+        property: 'role',
+        value: '{receiverRole}'
+      },
+      proxy: {
+        type: 'ajax',
+        extraParams: {
+          type: 'response'
+        },
+        url: '/api/documents/response_types',
+        reader: {
+          type: 'json'
+        }
+      },
+      listeners: {
+        load: 'onResponseTypesStoreLoad'
+      }
+    }
+  },
+
+  formulas: {
+    hideResult: function(get) {
+      return !get('isResult');
+    },
+
+    hideComplete: function(get) {
+      return get('selection.status') !== helpers.document.status.CURRENT;
+    },
+
+    saveLabel: function(get) {
+      if (get('isResult')) {
+        return i18n.document.comment.actions.saveResult;
+      }
+      return i18n.document.comment.actions.saveComment;
+    },
+
+    receiverRole: function(get) {
+      return get('selection.receiver_role') || 'owner';
+    }
+  }
+});
+
+Ext.define('Tel100.view.document.motions.ResultPanelViewController', {
+  extend: 'Ext.app.ViewController',
+  alias: 'controller.documentmotionsresultpanel',
+
+  onSaveClick: function(button, e, eOpts) {
+    var view = this.getView();
+    var vm = this.getViewModel();
+
+    var docId = vm.get('document').id;
+    var motionId = vm.get('selection.id');
+    if (typeof motionId !== 'number') { motionId = null; }
+    var text = vm.get('text');
+    var isComplete = vm.get('isResult');
+    if (isComplete && vm.get('hideComplete')) { isComplete = false; }
+    var responseTypeId = null;
+    if (isComplete) { responseTypeId = vm.get('categoryId'); }
+
+    var params = {
+      document_id: docId,
+      motion_id: motionId,
+      is_result: isComplete,
+      text: text,
+      response_type_id: responseTypeId
+    };
+
+    if (!params.is_result && !params.text) {
+      Ext.Msg.show({
+        title: i18n.document.comment.errors.text_required_title,
+        message: i18n.document.comment.errors.text_required,
+        buttons: Ext.Msg.OK,
+      });
+      return;
+    }
+
+    view.setLoading(true);
+
+    helpers.api.document.comment.create({
+      params: params,
+      success: function(data) {
+        view.resetForm();
+        view.setLoading(false);
+        view.fireEvent('commentadded');
+      },
+      failure: function(error) {
+        view.setLoading(false);
+        console.error(error);
+      }
+    });
+  },
+
+  onMotionsStoreLoad: function(store, records, successful, eOpts) {
+    var view = this.getView();
+    var combo = view.down('#in-motions');
+    var val = store.getAt(0);
+    combo.select(val);
+  },
+
+  onResponseTypesStoreLoad: function(store, records, successful, eOpts) {
+    var view = this.getView();
+    var combo = view.down('#result-types');
+    var val = store.getAt(0);
+    combo.select(val);
+  }
+});
