a3a908edd3683d1a9a35c7de41842d6b8d665f16
Dimitri Kurashvili <dimakura@gmail.com>
2015-07-16 10:55:42 +0400

    changes in comment model

--- app/models/document/comment.rb
+++ app/models/document/comment.rb
@@ -10,4 +10,19 @@ class Document::Comment < ActiveRecord::Base
   belongs_to :motion, class_name: 'Document::Motion', foreign_key: 'motion_id'
   belongs_to :user, class_name: 'Sys::User', foreign_key: 'user_id'
   belongs_to :actual_user, class_name: 'Sys::User', foreign_key: 'actual_user_id'
+  belongs_to :receiver_user, class_name: 'Sys::User', foreign_key: 'receiver_user_id'
+
+  before_create :calculate_receiver_user
+
+  def calculate_receiver_user
+    if self.motion.present?
+      user1 = motion.actual_sender
+      user2 = motion.last_receiver
+      if user1 == self.user || user1 == self.actual_user
+        self.receiver_user = user2
+      else
+        self.receiver_user = user1
+      end
+    end
+  end
 end
