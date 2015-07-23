5c2ae1ba7bdc73e17dd330cb6a3ddc13f3a4a999
Dimitri Kurashvili <dimakura@gmail.com>
2015-07-23 09:18:20 +0400

    document search

--- app/controllers/admin/documents_controller.rb
+++ app/controllers/admin/documents_controller.rb
@@ -2,5 +2,9 @@
 class Admin::DocumentsController < AdminController
   def index
     @title = 'დოკუმენტების მართვა'
+    if params[:docnumber].present?
+      @document = Document::Base.find_by_docnumber(params[:docnumber])
+      
+    end
   end
 end
