e1eb54fddd6eb6c7fa7421e3ad821a6cecbde948
T <opit@gol.ge>
2015-12-28 22:20:38 +0400

    change organization trigger

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921074640) do
+ActiveRecord::Schema.define(version: 20151228082625) do
 
   create_table "document_base", force: true do |t|
     t.string    "language",          limit: 2,                             default: "KA",    null: false
