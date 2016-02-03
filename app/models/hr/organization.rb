a7ae692e4364f3c691f43cf07a64fc898d9f4640
T <opit@gol.ge>
2016-02-03 12:31:56 +0400

    change hr_organization sequence in ruby

--- app/models/hr/organization.rb
+++ app/models/hr/organization.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 class HR::Organization < ActiveRecord::Base
   self.table_name  = 'hr_organizations'
-  self.sequence_name = 'hr_orgs_seq'
+  self.sequence_name = 'hr_employees_seq'
   self.localized_field('name')
   self.set_integer_columns :is_manager, :is_active, :custom
   belongs_to :parent, class_name: 'HR::Organization', foreign_key: 'parent_id'
