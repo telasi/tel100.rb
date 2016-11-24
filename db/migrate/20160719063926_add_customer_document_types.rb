5863ee2b997c914d5fa0059b46f270eff09bc991
T <opit@gol.ge>
2016-11-24 12:19:43 +0400

    change doc type

--- db/migrate/20160719063926_add_customer_document_types.rb
+++ db/migrate/20160719063926_add_customer_document_types.rb
@@ -1,7 +1,7 @@
 class AddCustomerDocumentTypes < ActiveRecord::Migration
   def up
     execute <<-SQL
-      insert into DOCUMENT_TYPE(NAME_KA, NAME_RU, ORDER_BY) VALUES ('მომხმარებელთა წერილი', 'Письмо пользователя', 13)
+      insert into DOCUMENT_TYPE(NAME_KA, NAME_RU, ORDER_BY) VALUES ('მომხმარებელთა განცხადება', 'Заявление пользователя', 13)
     SQL
     execute <<-SQL
       insert into DOCUMENT_TYPE(NAME_KA, NAME_RU, ORDER_BY) VALUES ('აღრიცხვის ხელსაწყოების შემოწმება', 'Проверка устройств учета', 14)
