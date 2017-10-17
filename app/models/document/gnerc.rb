65e6038b6a51d442412c64bc7c5d8602c1982127
T <opit@gol.ge>
2017-10-17 10:55:22 +0400

    reset type_id if document type is not gnerc 4 service

--- app/models/document/gnerc.rb
+++ app/models/document/gnerc.rb
@@ -35,6 +35,7 @@ class Document::Gnerc < ActiveRecord::Base
     document = Document::Base.find(params[:id])
     gnerc = Document::Gnerc.where(document: document).first || Document::Gnerc.new(document: document, status: 1)
     gnerc.update_attributes!(params.permit(:type_id, :status, :mediate))
+    gnerc.update_attributes!(type_id: nil) unless document.type_id == GNERC_TYPE4
     gnerc.save!
   end
 
