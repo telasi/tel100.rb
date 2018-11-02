dd147106fc93b9b8b1fac0d32174290f5527c242
temochutlashvili <opit@gol.ge>
2018-11-02 09:17:02 +0400

    napr integration fix

--- app/models/gnerc/sender.rb
+++ app/models/gnerc/sender.rb
@@ -181,10 +181,12 @@ class Gnerc::Sender
 
           GnercWorker.perform_async("answer", DOCFLOW_TO_GNERC_MAP[related.type_id], parameters)
 
-          if content.present?
-            JusticeWorker.perform_async({ docid: related.id, docnumber: related.docnumber, name: file.original_name, content: content })
-          else
-            JusticeWorker.perform_async({ docid: related.id, docnumber: related.docnumber, name: 'answer.txt', content: smsrecord.text })
+          if related.sender_id == JUSTICE_USER
+            if content.present?
+              JusticeWorker.perform_async({ docid: related.id, docnumber: related.docnumber, name: file.original_name, content: content })
+            else
+              JusticeWorker.perform_async({ docid: related.id, docnumber: related.docnumber, name: 'answer.txt', content: smsrecord.text })
+            end
           end
         end
       end
