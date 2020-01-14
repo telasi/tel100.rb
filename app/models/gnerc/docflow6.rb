ba1467cae2337cbbc9f880480ffaea7375e297ec
temochutlashvili <opit@gol.ge>
2020-01-14 12:09:43 +0400

    gnerc models

--- app/models/gnerc/docflow6.rb
+++ app/models/gnerc/docflow6.rb
@@ -2,5 +2,5 @@
 class Gnerc::Docflow6 < ActiveRecord::Base
   establish_connection :gnerc
   self.table_name  = 'docflow6'
-  self.set_integer_columns :affirmative, :public_service_hall
+  self.set_integer_columns :affirmative, :public_service_hall #, :response_id
 end
