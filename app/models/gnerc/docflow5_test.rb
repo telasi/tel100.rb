7e4e65fc1d3bd391ceed8eee63ab6b13c660e2f5
temochutlashvili <opit@gol.ge>
2020-01-16 16:21:43 +0400

    gnerc test models

--- app/models/gnerc/docflow5_test.rb
+++ app/models/gnerc/docflow5_test.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Gnerc::Docflow5Test < ActiveRecord::Base
   establish_connection :gnerc_test
-  self.table_name  = 'docflow5'
+  self.table_name  = 'temo.docflow5'
   self.set_integer_columns :affirmative, :mediate, :response_id
 end
