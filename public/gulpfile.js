8a1ef760170cf2aa10fa062e597308c71fffcc97
temochutlashvili <opit@gol.ge>
2021-05-28 14:57:40 +0400

    new gulp

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -20,47 +20,74 @@ var options = minimist(process.argv.slice(2), knownOptions);
  * Task for building all targets.
  */
 
-gulp.task('all', ['i18n.js', 'helpers.js', 'tel100.js', 'index.html']);
+// gulp.task('all', gulp.parallel('i18n.js', 'helpers.js', 'tel100.js', 'index.html'));
+// gulp.task('default', gulp.parallel('i18n.js', 'helpers.js', 'tel100.js', 'index.html'));
 
 /**
  * 
  */
 
-gulp.task('default', function () {
-  gulp.watch('./i18n/**/*.js', [ 'i18n.js', 'index.html' ]);
-  gulp.watch('./helpers/**/*.js', [ 'helpers.js', 'index.html' ]);
-  gulp.watch('./tel100/app/**/*.js', [ 'tel100.js', 'index.html' ]);
-});
-
-gulp.task('tel100.js', function() {
+gulp.task('tel100.js', function(cb) {
   gulp.src('./tel100/app/**/*.js')
     .pipe(removeRequireDirectives())
     .pipe(concat('tel100.js'))
     .pipe(gulpif(options.production, uglify()))
     .pipe(gulp.dest('./build'));
+  cb();
 });
 
-gulp.task('i18n.js', function() {
+gulp.task('i18n.js', function(cb) {
   gulp.src('./i18n/index.js')
     .pipe(browserify())
     .pipe(concat('i18n.js'))
     .pipe(gulpif(options.production, uglify()))
     .pipe(gulp.dest('./build'));
+  cb();
 });
 
-gulp.task('helpers.js', function() {
+gulp.task('helpers.js', function(cb) {
   gulp.src('./helpers/index.js')
     .pipe(browserify())
     .pipe(concat('helpers.js'))
     .pipe(gulpif(options.production, uglify()))
     .pipe(gulp.dest('./build'));
+  cb();
 });
 
-gulp.task('index.html', function() {
+gulp.task('index.html', function(done) {
   var indexHtml = swig.renderFile('index.swig', {
     version: new Date().getTime()
   });
   fs.writeFileSync('index.html', indexHtml);
+  done();
+});
+
+function tel100(){ 
+  return gulp.src('./tel100/app/**/*.js')
+    .pipe(removeRequireDirectives())
+    .pipe(concat('tel100.js'))
+    .pipe(gulpif(options.production, uglify()))
+    .pipe(gulp.dest('./build'));
+};
+
+function index(done){
+  var indexHtml = swig.renderFile('index.swig', {
+    version: new Date().getTime()
+  });
+  fs.writeFileSync('index.html', indexHtml);
+  done();
+}
+
+gulp.task('default', function (cb) {
+  gulp.watch('./i18n/**/*.js', gulp.series( 'i18n.js', 'index.html' ));
+  gulp.watch('./helpers/**/*.js', gulp.series( 'helpers.js', 'index.html' ));
+  gulp.watch('./tel100/app/**/*.js', gulp.series('tel100.js', 'index.html' ));
+  cb();
+});
+
+gulp.task('all', function(done) {
+  gulp.series('i18n.js', 'helpers.js', 'tel100.js', 'index.html');
+  done();
 });
 
 /**
@@ -80,7 +107,7 @@ function removeRequireDirectives() {
       if (!requireBlock) { newLines.push(line); }
       if (line.indexOf('],') !== -1) { requireBlock = false; }
     }
-    file.contents = new buffer.Buffer(newLines.join('\n').trim(), 'utf-8');
+    file.contents = buffer.Buffer.from(newLines.join('\n').trim(), 'utf-8');
     cb(null, file);
   };
 
